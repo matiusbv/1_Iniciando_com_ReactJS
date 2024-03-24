240164464d119b1ff5d52c1decd062735552ba90
Marcio <matiusbv@gmail.com>
2024-03-24 17:35:44 -0300

    4 Componente sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,17 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello world</h1>
-      <Post 
+  <div className={styles.wrapper}>
+    <Sidebar />
+    <main>
+    <Post 
         author="Márcio Bezerra"
         content="conteúdo do autor Márcio"
       />
@@ -16,6 +20,8 @@ function App() {
         author="Diego Fernandes"
         content="Conteúdo do autor Diego"
       />
+    </main>
+  </div>
     
     </>
   )
