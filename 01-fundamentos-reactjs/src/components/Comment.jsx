3687ded938d1ae75a5657ba0e0bca7aa2c224862
Marcio <matiusbv@gmail.com>
2024-05-13 20:44:34 -0300

    Estilizacoes do comentario

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -13,7 +13,7 @@ export function Comment() {
                             <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
                         </div>
                         <button title='Deletar Comentário'>
-                            <Trash size={20}/>
+                            <Trash size={24}/>
                         </button>
                     </header>
 
