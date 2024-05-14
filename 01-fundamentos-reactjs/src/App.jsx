e1a59d9acee30037560938f7e964e9270a5c3faa
Marcio <matiusbv@gmail.com>
2024-05-14 17:44:59 -0300

    Iterando no JSX

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -4,6 +4,39 @@ import styles from './App.module.css'
 import { Sidebar } from "./components/Sidebar"
 import './global.css'
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
+
 function App() {
   return (
     <>
@@ -11,15 +44,17 @@ function App() {
   <div className={styles.wrapper}>
     <Sidebar />
     <main>
-    <Post 
-        author="Márcio Bezerra"
-        content="conteúdo do autor Márcio"
-      />
+        {posts.map(post =>{
+          return(
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        })
 
-      <Post 
-        author="Diego Fernandes"
-        content="Conteúdo do autor Diego"
-      />
+        }
     </main>
   </div>
     
