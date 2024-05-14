fcf83046043f317bea55c8a78eea47c4b97a26b0
Marcio <matiusbv@gmail.com>
2024-05-13 21:21:02 -0300

    Componente Avatar

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,10 +1,11 @@
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css'
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment() {
     return(
         <div className={styles.comment}>
-            <img src="https://github.com/matiusbv.png" alt="" />
+            <Avatar hasBorder={false} src="https://github.com/maykbrito.png" alt="" />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
